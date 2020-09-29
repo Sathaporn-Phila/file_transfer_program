93732075eacbc3703a3d07da60799de736af9b3f
littleprogrammer-r5r5 <s6201012630037@email.kmutnb.ac.th>
2020-09-29 13:31:47 +0700

    add comment

--- Basic_WebSocket_Server/Client.py
+++ Basic_WebSocket_Server/Client.py
@@ -3,11 +3,11 @@ import asyncio
 
 async def hello():
     uri = "ws://localhost:8000"
-    async with websockets.connect(uri) as websocket:
+    async with websockets.connect(uri) as websocket: # connect to server
         print("You are now connecting with a server.")
         message = input(">>>")
-        await websocket.send(message)
-        response = await websocket.recv()
+        await websocket.send(message) # send message to server
+        response = await websocket.recv() # receive message from server
         print(response)
 
 asyncio.get_event_loop().run_until_complete(hello())
--- Basic_WebSocket_Server/Server.py
+++ Basic_WebSocket_Server/Server.py
@@ -2,10 +2,10 @@ import websockets
 import asyncio
 
 async def hello(websocket, path):
-    message = await websocket.recv()
+    message = await websocket.recv() # receive message from client
     print(message)
     greeting = f"This message is from server. It receive message from you. It is {message}"
-    await websocket.send(greeting)
+    await websocket.send(greeting) # send message back to client
 
 start_server = websockets.serve(hello, "localhost", 8000)
 
