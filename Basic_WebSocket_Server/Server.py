ec230e86736d27d89631210d73d783782caf1845
nongboom <68316674+littleprogrammer-r5r5@users.noreply.github.com>
2020-09-29 13:37:16 +0700

    Update Server.py

--- Basic_WebSocket_Server/Server.py
+++ Basic_WebSocket_Server/Server.py
@@ -4,7 +4,8 @@ import asyncio
 async def hello(websocket, path):
     message = await websocket.recv() # receive message from client
     print(message)
-    greeting = f"This message is from server. It receive message from you. It is {message}"
+    greeting = f'''This message is from server. 
+    It receive message from you. It is {message}'''
     await websocket.send(greeting) # send message back to client
 
 start_server = websockets.serve(hello, "localhost", 8000)
