2e1fe6126a7c5c5c636286decb3341d84771e272
Sathaporn01 <68017402+Sathaporn01@users.noreply.github.com>
2020-10-04 14:35:37 +0700

    Add files via upload

--- image_create.py
+++ image_create.py
@@ -1,8 +1,12 @@
-from PIL import Image,ImageFont,ImageDraw
+from PIL import Image,ImageFont,ImageDraw,ImageQt
+def user_image(text,color):
+    return create_image(("Username : " + text),color)
+def content_image(text,color):
+    return create_image(text,color)
 def create_image(text,color):
-    # if text are in web server file ,load it else: create new file
+    # if text are in web server file ,load it  else: create new file
     img = Image.new("RGB",(600,300),color)
-    name = "Username : " + text
+    name = text
     font = ImageFont.truetype("times.ttf",40)
     width,height = font.getsize(name)
     
@@ -12,5 +16,4 @@ def create_image(text,color):
     img = Image.open("{}.png".format(text))
     return img
 #test
-#a = create_image("Nuieasy","blue")
-#a.show()
+
--- index_page.py
+++ index_page.py
@@ -1,107 +1,107 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-import sys
+from PIL.ImageQt import ImageQt
+import sys,image_create
 
-class App(QWidget):
-    border = 20
-    normal_font_size = 20
-    # initialized value before runs application
-    def __init__(self):
+class Button(QPushButton):
+
+    def __init__(self,parent,text):
+        super().__init__(parent)
+        self.text = text
+class TreeView(QTreeView):
+
+    def __init__(self,parent):
+        super().__init__(parent)
+        super().setHeaderHidden(True)
+
+class Item(QStandardItem):
+
+    def __init__(self,text = "",color = QColor(Qt.black),fontSize = 20):
         super().__init__()
-        self.screen_width = 1366
-        self.screen_height = 768
-        self.title = "File transfer program"
-        self.app_width = 800
-        self.app_height = 600
-        self.initial_x = (self.screen_width - self.app_width)//2
-        self.initial_y = (self.screen_height - self.app_height)//2
-        self.init_gui()
+        font = QFont("Times New Roman",fontSize)
+        self.setEditable(False)
+        self.setFont(font)
+        self.setText(text)
+
+class TreeModel(QStandardItemModel):
+
+    def __init__(self,parent):
+        super().__init__(parent)
+        self.root_node = super().invisibleRootItem
+
+class Header :
+
+    def __init__(self,user_name):
+        self.user_image = ImageQt(image_create.user_image(user_name,"darkblue"))
     
-    def init_gui(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.initial_x, self.initial_y, self.app_width, self.app_height)
-        self.search_box()
+class PublicPage(QWidget):
+
+    def __init__(self,user_name):
+        self.app = QApplication(sys.argv)
+        super().__init__()
+        self.user_name = user_name
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle("File transfer system")
+        self.center()
+        self.set_gui()
         self.show()
+        sys.exit(self.app.exec_())
 
-    def paintEvent(self,event):
-        self.qpaint = QPainter()
-        self.qpaint.begin(self)
-        top = self.header()
-        file_transfer_button = self.prepare_file_transfer_button()
-        slide_tab = self.slide_bar()
-        show_event = self.main_show_event()
-        self.qpaint.end()
+    def center(self):
+        frame_geometry = self.frameGeometry()
+        center_point = QDesktopWidget().availableGeometry().center()
+        frame_geometry.moveCenter(center_point)
+        self.move(frame_geometry.topLeft())
 
-    def header(self):
-        self.header_font = self.app_height//16
-        self.header_width = self.app_width-2*self.border
-        self.header_height = self.app_height//4
+    def set_gui(self):
 
-        self.qpaint.setPen(QColor(Qt.blue))
+        self.line = 0
+
+        content_button = ["Public", "Inbox", "Send", "Export", "Import","Log out"]
+        self.header_image = self.image(Header(self.user_name).user_image)
+        self.vertical_layout = QVBoxLayout()
+        self.vertical_layout.addWidget(self.header_image)
+
+        self.main_zone = QGridLayout()
+        for num in range(len(content_button)):
+            item = Button(self,content_button[num])
+            item.setStyleSheet("background-color : rgb(0,255,0);")
+            item.setText(item.text)
+            self.main_zone.addWidget(item,num*2,0,2,2)
+            self.line += num
+
+        self.main_zone.addWidget(TreeView(self),0,3,2*len(content_button),2*len(content_button))
         
-        self.qpaint.drawRect(self.border,self.border,self.header_width,self.header_height)
-        self.qpaint.fillRect(self.border,self.border,self.header_width,self.header_height,QBrush(Qt.blue))
+        find_label = QLabel()
+        find_label.setText("find : ")
+        find_label.setAlignment(Qt.AlignCenter)
+        self.main_zone.addWidget(find_label,self.line+1,0,2,2)
+        self.main_zone.addWidget(QLineEdit(self),self.line+1,3,2,2*(len(content_button)-1))
 
-        self.qpaint.setPen(QColor(Qt.white))
-        self.qpaint.setFont(QFont('Times New Roman',self.header_font))
-        self.qpaint.drawText(self.border*2,self.border+self.header_height//2,"Username:..........")
+        self.search_button_function_name = ["----->","Reset"]
+        
+        for num in range(len(self.search_button_function_name)):
+            item = Button(self,self.search_button_function_name[num])
+            item.setStyleSheet("background-color : rgb(0,255,100);")
+            item.setText(item.text)
+            self.main_zone.addWidget(item,self.line+1,3+2*(len(content_button)-1)+num,1,1)
+        
+        
+        self.vertical_layout.addLayout(self.main_zone)
+        self.setLayout(self.vertical_layout)
 
-    def prepare_file_transfer_button(self):
-        self.transfer_button_width = self.header_height//2 - self.border
-        self.transfer_button_height = self.header_height - 2*self.border
-        x = self.app_width - 2*self.border -  self.transfer_button_width
-        y = 2*self.border
-        self.qpaint.setPen(QColor(Qt.white))
-        self.qpaint.drawRect(x,y,self.transfer_button_width,self.transfer_button_height)
-        self.qpaint.fillRect(x,y,self.transfer_button_width,self.transfer_button_height,QBrush(Qt.white))
+    def image(self,picture):
+        self.new_image = QLabel("",self)
+        new_image = QPixmap.fromImage(picture)
+        self.new_image.setPixmap(new_image.scaled(new_image.width()//2,new_image.height()//2))
+        self.new_image.setSizePolicy(QSizePolicy.Preferred,QSizePolicy.Preferred)
+        self.new_image.setAlignment(Qt.AlignCenter)
+        return self.new_image
 
-        self.qpaint.setPen(QColor(Qt.black))
-        self.qpaint.setFont(QFont('Times New Roman',self.header_font))
-        self.qpaint.drawText(x+self.transfer_button_width//4,y+self.transfer_button_height//2,"+")
-
-    def slide_bar(self):
-        x = self.border
-        y = self.header_height+ 2*self.border
-        self.button_width = self.header_width//4
-        self.button_height = (self.app_height - y - self.border)//3
-        num_button = 3
-        self.qpaint.setPen(QColor(Qt.black))
-        for num_item in range(num_button):
-            self.qpaint.drawRect(x,y,self.button_width,self.button_height)
-            self.qpaint.fillRect(x,y,self.button_width,self.button_height,QBrush(Qt.white))
-            y += self.button_height + 5
-
-        y_mid = self.header_height+ 2*self.border + self.button_height//2
-        x_mid = self.border + self.button_width//2
-        self.qpaint.setPen(QColor(Qt.black))
-        self.qpaint.setFont(QFont('Times New Roman',20))
-        name_button = ["public","inbox","send"]
-        for num_item in range(len(name_button)):
-            self.qpaint.drawText(x_mid,y_mid,name_button[num_item])
-            y_mid += self.button_height
-
-    def main_show_event(self):
-        x = self.button_width + 2*self.border
-        y = self.header_height+ 2*self.border
-        main_show_width = self.app_width - self.border - x
-        main_show_height = self.app_height - y - 2*self.border - self.button_height//2
-        self.qpaint.setPen(QColor(Qt.red))
-        self.qpaint.drawRect(x,y,main_show_width,main_show_height)
-
-    def search_box(self):
-        button_height = (self.app_height - self.app_height//4 - 3*self.border)//3
-        x = self.app_width//4 + 2*self.border
-        y = self.app_height - self.border - button_height//2
-        self.textbox = QLineEdit(self)
-        self.textbox.move(x,y)
-        self.textbox_width = self.app_width - 4*self.border - x 
-        self.textbox_height = button_height//2
-        self.textbox.resize(self.textbox_width,self.textbox_height)
-
-def run_app():
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
-
-run_app()
+ex = PublicPage("Nuieasy")
+   
+        
+    
