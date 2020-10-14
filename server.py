d8b50d815625ffcc7141a58ab089d0c4c4dd780a
littleprogrammer-r5r5 <s6201012630037@eamil.kmutnb.ac.th>
2020-10-14 10:06:49 +0700

    direct and public send

--- server.py
+++ server.py
@@ -3,12 +3,25 @@ import asyncio
 import sys
 from class_file import *
 
+client_dict = {}
 async def recvfile(websocket, path):
-    filename = await websocket.recv()
-    mode = await websocket.recv()
-    f = File(filename)
-    f.write(mode)
-
+    client_set.append(websocket)
+    while True:
+        client_username = await websocket.recv()
+        client_dict[client_username] = websocket
+        comm = await websocket.recv()
+        if comm == 'send_direct':
+            filename = await websocket.recv()
+            mode = await websocket.recv()
+            dest = await websocket.recv()
+            recv_line = await websocket.recv()
+            while recv_line:
+                await client_dict[dest].send(recv_line)
+        elif comm == 'send_public':
+            filename = await websocket.recv()
+            mode = await websocket.recv()
+            f = File(filename)
+            f.write(mode)
 start_server = websockets.serve(recvfile, "localhost", 8000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
