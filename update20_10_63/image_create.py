73ea28db41b98e6b91b0d992d79149b08e9d3ec6
Sathaporn01 <68017402+Sathaporn01@users.noreply.github.com>
2020-10-21 00:55:31 +0700

    Add files via upload

--- update20_10_63/image_create.py
+++ update20_10_63/image_create.py
@@ -1,12 +1,10 @@
 from PIL import Image,ImageFont,ImageDraw,ImageQt
 def user_image(text,color):
-    return create_image(("Username : " + text),color)
-def content_image(text,color):
     return create_image(text,color)
 def create_image(text,color):
     # if text are in web server file ,load it  else: create new file
     img = Image.new("RGB",(600,300),color)
-    name = text
+    name = "Username : " + text
     font = ImageFont.truetype("times.ttf",40)
     width,height = font.getsize(name)
     
